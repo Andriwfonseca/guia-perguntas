d87acb11d045d8b7716c8269640b0fd0ff39b86e
Andriw Fonseca <filipefonsequinha@gmail.com>
2022-04-13 17:25:12 -0300

    add validation to create resposta

--- controllers/respostaController.js
+++ controllers/respostaController.js
@@ -1,17 +1,26 @@
 const Resposta = require('../models/Resposta');
+const Pergunta = require('../models/Pergunta');
 
 const responder = (req, res) => {
     const { corpo } = req.body;
     const id_pergunta = req.body.pergunta;
 
-    Resposta.create({
-        corpo,
-        id_pergunta
-    }).then(() => {
-        res.redirect('/pergunta/' + id_pergunta);
-    });;
-}
+    Pergunta.findOne({
+        where: {id: id_pergunta}
+    }).then(pergunta => {
+        if(pergunta){
 
+            Resposta.create({
+                corpo,
+                id_pergunta
+            }).then(() => {
+                res.redirect('/pergunta/' + id_pergunta);
+            });
+        }else{
+            res.redirect('/');
+        }
+    });
+}
 module.exports = {
     responder
 }
