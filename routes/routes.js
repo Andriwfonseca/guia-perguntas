0bff844e2e343754e099c37d3c6657ecc03454f1
Andriw Fonseca <filipefonsequinha@gmail.com>
2022-04-13 16:29:55 -0300

    add resposta routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const perguntaController = require('../controllers/perguntaController');
+const respostaController = require('../controllers/respostaController');
 
 const router = Router();
 
 router.get('/', perguntaController.findAll);
 router.get('/perguntar', perguntaController.perguntar);
+router.get('/pergunta/:id', perguntaController.perguntaID);
 router.post('/salvarpergunta', perguntaController.salvarPergunta);
+
+router.post('/responder', respostaController.responder);
 
 module.exports = router;
